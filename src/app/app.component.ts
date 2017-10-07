f2771d94a6e013590af874b03da2b7159a2ae246
nealhartley <neal.w.hartley@gmail.com>
2017-10-08 01:21:56 +1300

    Added floating icons that link to cv and linkedin, added css animations to these icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // lets add an animation
+  animations: [
+    trigger('SlideInOut', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(120%,0,0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'app';
+  menuState:string = 'out';
+  toggleMenu() {
+    // if statement that toggles the value
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
+import { ResearchComponent } from './research/research.component';
+import { TeachingComponent } from './teaching/teaching.component';
+import { ConsultingComponent } from './consulting/consulting.component';
+import { HonoursComponent } from './honours/honours.component';
+import { PhdComponent } from './phd/phd.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+let routes: Routes = [
+  {path: 'education', component: EducationComponent},
+  {path: 'research', component: ResearchComponent},
+  {path: 'teaching', component: TeachingComponent},
+  {path: 'consulting', component: ConsultingComponent},
+  {path: 'honours', component: HonoursComponent},
+  {path: 'phd', component: PhdComponent},
+  {path: '', component: HomeComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    ResearchComponent,
+    TeachingComponent,
+    ConsultingComponent,
+    HonoursComponent,
+    PhdComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
